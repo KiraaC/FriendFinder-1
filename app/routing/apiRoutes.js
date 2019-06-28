e7d4c13caa5af57a84d01342ac901c59f429c3ab
unknown <cmzolczynski@gmail.com>
2019-06-28 10:56:08 -0700

    set up port and routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+// displays all friends
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+});
+
+app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newfriends = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newfriends.routeName = newfriends.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newfriends);
+  
+    characters.push(newfriends);
+  
+    res.json(newfriends);
+  });
